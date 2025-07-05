8e6dd6e8b71529e07e3a91857f1ba7f1e3f6af11
Fady El Gawly <fadyelgawly@lokaleg.com>
2025-07-05 06:28:44 +0300

    iterate over most generaic

--- main.py
+++ main.py
@@ -3,49 +3,26 @@ from n8n import start_n8n_container
 from utils import env_vars, local_timezone
 from utils import run_command
 
-
-# INSTALL DOCKER (if needed) 
+# INSTALL DOCKER (if needed)
 install_docker()
 print("""
-There is no undo functionality. If you enter a question wrong and submit it you must run the script again with the original command.
-
 Reminder: You will have keys in a .env file on your system after this proccess. You are responsible to secure it.
 """)
 
-
-is_custom_image = False
-list_of_packages = []
-
-
-# ------------------------------------------------------------------------
-# -------------------      GENERIC CONFIGURATION      --------------------
-# ------------------------------------------------------------------------
-
-# Sets var for detailed setup type
-detailed_setup = False
-
-# Sets URL
-domain = "localhost"
-env_vars['N8N_EDITOR_BASE_URL'] = f'http://{domain}:5678'
-env_vars['WEBHOOK_URL'] = f'http://{domain}:5678'
-
-
-# Sets Timezone
+# --- GENERIC CONFIGURATION ---
+env_vars['N8N_EDITOR_BASE_URL'] = 'http://localhost:5678'
+env_vars['WEBHOOK_URL'] = 'http://localhost:5678'
 env_vars["GENERIC_TIMEZONE"] = str(local_timezone) or "America/New_York"
 
-
+# --- Start n8n ---
 print("\nstarting n8n...")
-start_n8n_container(env_vars, is_custom_image, list_of_packages)
+start_n8n_container(env_vars, is_custom_image=False, list_of_packages=[])
 print("n8n started")
-
 
 print("""
 Thank you for using my tool!
-Please give me feedback if you have any!
-      
-liam@teraprise.io
 """)
 
-# TODO: add cleanup scripts here
+# --- Cleanup ---
 print("deleting temp folder...")
 run_command("rm -rf n8n-auto-install")
